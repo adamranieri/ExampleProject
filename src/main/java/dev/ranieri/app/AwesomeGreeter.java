f078ee513e3e611ef60d18b1cec67794249dd4ab
AdamRanieri <adaman94@gmail.com>
2022-04-18 12:24:33 -0400

    made some new changes to my AwesomeGreeter added French.

--- src/main/java/dev/ranieri/app/AwesomeGreeter.java
+++ src/main/java/dev/ranieri/app/AwesomeGreeter.java
@@ -5,4 +5,8 @@ public class AwesomeGreeter {
     public static void hello(){
         System.out.println("Hello!!!!!!");
     }
+
+    public static void bonjour(){
+        System.out.println("Bonjour!!!!");
+    }
 }
